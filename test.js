0eb472f6929c68b2bcc34c52d24f4840e9eab33a
brandonpotvin <brandon.potvin@gmail.com>
2022-12-15 16:38:21 -0500

    Update test.js

--- test.js
+++ test.js
@@ -1,7 +1,5 @@
 //send user email to clipboard
 
-alert('Hello World')
-
 function copyToClipboard(clip) {
     var copyText = clip;
     navigator.clipboard.writeText(copyText).then(() => {
@@ -11,8 +9,8 @@ function copyToClipboard(clip) {
     });
   }
 
-// var email_address = document.querySelector("#user_info_1-tr > td > div > div > center > table > tbody > tr:nth-child(3) > td:nth-child(2) > p > b > span.piping_receiver.piperec-4200-user_email").textContent
+var email_address = document.querySelector("#user_info_1-tr > td > div > div > center > table > tbody > tr:nth-child(3) > td:nth-child(2) > p > b > span.piping_receiver.piperec-4200-user_email").textContent
 
-// alert(email_address)
+alert(email_address)
 
-// copyToClipboard(email_address)
+copyToClipboard(email_address)
