4a70737290f782efa00130be6f877ae45a491241
brandonpotvin <brandon.potvin@gmail.com>
2022-12-22 10:19:02 -0500

    Update ump.js

--- rc/ump.js
+++ rc/ump.js
@@ -43,7 +43,7 @@ if(page == "sams_aa"){
     navigator.clipboard.writeText(copyText).then(() => {
         // Alert the user that the action took place.
         // Nobody likes hidden stuff being done under the hood!
-        alert("Copied " + clip + " to clipboard");
+        console.log("Copied " + clip + " to clipboard");
     });
   }
 
