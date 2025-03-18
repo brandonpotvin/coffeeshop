2c1b4bafc7053f2156a84ed933b254df3d3881e9
brandonpotvin <brandon.potvin@gmail.com>
2025-03-18 18:49:08 -0400

    Update and rename sams_search.js to sams_search_activity.js

--- rc/sams_search_activity.js
+++ rc/sams_search_activity.js
@@ -1,3 +1,8 @@
+// Open Page Assign Activity
+window.open("https://im.cdc.gov/iam/im/SAMS3/ui/index.jsp?task.tag=AssignActivityAccess");
+
+
+
 //Select option to serach by email, paste email from clipboard and start search
 
 document.querySelector("#Filter\\.0\\.Field").selectedIndex = 4 //set search option to email
