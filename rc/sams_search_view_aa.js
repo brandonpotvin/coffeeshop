1da07e36d86205763ec87454d60f425f2bf2261c
brandonpotvin <brandon.potvin@gmail.com>
2025-03-18 20:46:00 -0400

    Update sams_search_view_aa.js

--- rc/sams_search_view_aa.js
+++ rc/sams_search_view_aa.js
@@ -1,22 +1,38 @@
-// Open Page View AA
+// SAMS Helper Script
+// This script checks if the current URL is a SAMS URL
+// If it is, it reads from clipboard and fills the specified input field
+// If not, it redirects to the SAMS URL
 
-url = "https://im.cdc.gov/iam/im/SAMS3/ui/index.jsp?task.tag=SAMSViewUserAA"
-window.open(url);
-
-//Select option to serach by email, paste email from clipboard and start search
-
-document.querySelector("#Filter\\.0\\.Field").selectedIndex = 4 //set search option to email
-
-function readFromClipboard(){
-    navigator.clipboard
-    .readText()
-    .then((copiedText) => {
-            // alert(copiedText)
-            document.querySelector("#Filter-queryFilter > tbody > tr > td:nth-child(4) > input[type=text]").value = copiedText //paste email
-
-   });
-}
-
-readFromClipboard()
-
-document.querySelector("#imh_1").click(); //click search button
+(function() {
+    // Replace this with the actual SAMS URL you want to use
+    const samsUrl = "https://im.cdc.gov/iam/im/SAMS3/ui/index.jsp?task.tag=SAMSViewUserAA";
+    
+    // Check if the current URL matches the SAMS URL
+    if (window.location.href.includes(samsUrl)) {
+        // We're on the right page, now try to read from clipboard
+        navigator.clipboard.readText()
+            .then(text => {
+                // Set the value to the specified filter input field
+                const inputField = document.querySelector("#Filter-queryFilter > tbody > tr > td:nth-child(4) > input[type=text]");
+                if (inputField) {
+                    inputField.value = text;
+                    console.log("Clipboard content pasted into the filter field");
+                    
+                    // Optional: Trigger any necessary events
+                    // This helps if the site uses event listeners to detect input changes
+                    const inputEvent = new Event('input', { bubbles: true });
+                    const changeEvent = new Event('change', { bubbles: true });
+                    inputField.dispatchEvent(inputEvent);
+                    inputField.dispatchEvent(changeEvent);
+                } else {
+                    alert("Could not find the filter input field on this page");
+                }
+            })
+            .catch(error => {
+                alert("Could not access clipboard. Error: " + error);
+            });
+    } else {
+        // Not on the SAMS URL, redirect
+        window.location.href = samsUrl;
+    }
+})();
