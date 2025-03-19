eee2af54e1888e9e00567f7dcc1b503ff05b1bbc
brandonpotvin <brandon.potvin@gmail.com>
2025-03-18 20:21:56 -0400

    Update sams_search_invite_user.js

--- rc/sams_search_invite_user.js
+++ rc/sams_search_invite_user.js
@@ -1,4 +1,9 @@
-javascript:(function() {
+// SAMS Helper Script
+// This script checks if the current URL is a SAMS URL
+// If it is, it reads from clipboard and fills the USER_ID field
+// If not, it redirects to the SAMS URL
+
+(function() {
     // Replace this with the actual SAMS URL you want to use
     const samsUrl = "https://im.cdc.gov/iam/im/SAMS3/ui/index.jsp?task.tag=SAMSInviteUser";
     
